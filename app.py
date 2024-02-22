50a8b6d6f2b409b93f9aacbde1ce9dcf995db3e7
Alan210716 <153419688+Alan210716@users.noreply.github.com>
2024-02-22 10:02:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,24 @@ def main():
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
     return(render_template("image_gpt.html"))
+@app.route("/text_result",methods=["GET","POST"])
+q=request.form.get("q")
+r = model.chat.completions.create(
+    model="gpt-3.5-turbo", 
+    messages=[
+        {"role": "user",
+         "content": q
+        }
+    ],
+)
+ r = replicate.run(
+    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    input={
+        "prompt": q,
+        }
+    )
+    time.sleep(10)
+    return(render_template("text_result.html",r=r[0]))
 
 @app.route("/image_result",methods=["GET","POST"])
 def image_result():
