762b713589179a47c1c2f17eaaf84cf482839638
Alan210716 <153419688+Alan210716@users.noreply.github.com>
2024-02-22 10:56:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from openai import OpenAI
 
 openai_api_key=os.getenv("OPENAI_API_KEY")
 os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
-
-model = OpenAI(api_key=openai_api_key)
+model = OpenAI(api_key="sk-qx4zKKzHnYRc7k8LuXyKHXdQi0gf5BcisgXBVZcXc9t4K8E9",
+               base_url="https://api.chatanywhere.tech/v1")
 
 app = Flask(__name__)
 
